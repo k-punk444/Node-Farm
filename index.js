91ad3548da32be76f624209fe374de7eae1a51c1
DevKai <irakoseimei4@gmail.com>
2023-10-19 07:30:03 +0900

    creating a simple web server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
+const http = require("http");
 
-// //Blocking, synchronous way
+////////////////////////////////////////////////////////////////
+// File
+
+// Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
@@ -23,3 +27,14 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   });
 }); //2
 console.log("Will read file!"); //1
+
+////////////////////////////////////////////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
